c5da7a803c2f993fe39c0ab97db20cbefbef8c60
greenhornsr <srollins11279@gmail.com>
2019-04-22 13:37:29 -0700

    built header components, styled with vanilla css

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+function HeaderContainer(){
+    return (
+        <section className='HeaderContainer'>
+            <ImageThumbnail />
+            <HeaderContent />
+        </section>
+    )
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(){
+    return (
+        <section className="HeaderTitle">
+            <h1>Lambda School<a href="mailto: lambda@LambdaSchool.com"> @LambdaSchool</a><time dateTime="2019-01-26"> - 26 Jan</time></h1> 
+        </section>
+    )
+}
+
+
+
+export default HeaderTitle;
