334fa21835be994f899f7f84b4e1391cf9d3e015
greenhornsr <srollins11279@gmail.com>
2019-04-22 15:12:33 -0700

    completed stretch, added moment.js, added footer icons from 3rd party library & embed them as links to be clicked on.  Project complete

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Footer.css';
 
+
 function Footer(){
     return(
         <section className="Footer">
-            
+
+            <span><a href=""><i class="far fa-comments img-icons"></i></a></span>
+            <span><a href=""><i class="fas fa-sync-alt img-icons"></i></a></span>
+            <span><a href=""><i class="far fa-heart img-icons"></i></a></span>
+            <span><a href=""><i class="far fa-envelope img-icons"></i></a></span>            
         </section>
     )
 }
